627d03169c32331635ea91f3e86ea27234c085fa
Roua Chehata <rouachehata06@gmail.com>
2026-05-30 00:07:07 +0100

    Update security mode check to allow all hours

--- backend/app.py
+++ backend/app.py
@@ -33,7 +33,7 @@ def check_security_mode(in_port, conn):
     if not in_port:
         return
     current_hour = datetime.now().hour
-    if 13 <= current_hour < 17 :
+    if current_hour >= 0:
         c = conn.cursor()
         c.execute("""SELECT * FROM alertes WHERE type='Mode Sécurité' 
                      AND date(timestamp)=date('now')""")
